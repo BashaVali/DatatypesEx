7d10d615bb7f2634d080dabe1a849928aa5db37d
BashaVali <bashavali222@gmail.com>
2022-11-30 05:32:19 +0530

    First Example

---
+++ DatatypesEx/DatatypesEx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatatypesEx
+{
+    internal class DatatypesEx
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Hello World !");
+            Console.Write("Hii");
+            Console.WriteLine("Good Morning All");
+        }
+    }
+}
